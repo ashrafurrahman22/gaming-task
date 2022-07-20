07fe73f8a02d4744b827fc774dc6129616e23a40
ashrafur22 <ashrafurrahman22@gmail.com>
2022-07-20 18:24:06 +0600

    tournaments-compo-updated

--- src/Pages/Home/Tournaments.js
+++ src/Pages/Home/Tournaments.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import './Styles/Tournaments.css';
+import image1 from '../../assets/Rectangle 466.png';
+import image2 from '../../assets/Rectangle 467.png';
+import image3 from '../../assets/Rectangle 468.png';
+import image4 from '../../assets/Rectangle 469.png';
+import image5 from '../../assets/Rectangle 471.png';
+import image6 from '../../assets/Rectangle 472.png';
+import image7 from '../../assets/Rectangle 473.png';
+import image8 from '../../assets/Rectangle 474.png';
+import image9 from '../../assets/Rectangle 487.png';
+import image10 from '../../assets/Rectangle 488.png';
+import image11 from '../../assets/Rectangle 491.png';
+import image12 from '../../assets/Rectangle 492.png';
 
 const Tournaments = () => {
     return (
@@ -18,6 +30,46 @@ const Tournaments = () => {
             </div>
 
             {/* image part */}
+            <div className='mesorny'>
+                <div className='grid-images'>
+                    <img src={image1} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image2} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image3} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image4} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image5} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image6} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image7} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image8} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image9} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image10} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image11} alt="" />
+                </div>
+                <div className='grid-images'>
+                    <img src={image12} alt="" />
+                </div>
+
+            </div>
+        
         </div>
     );
 };
